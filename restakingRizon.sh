8bbc0405d3d9e76ed6e71a4642085547eeabd147
didbit <pavel.didkovsky@gmail.com>
2021-10-11 11:38:53 +0200

    Update restakingRizon.sh

--- restakingRizon.sh
+++ restakingRizon.sh
@@ -2,16 +2,16 @@
 GREEN_COLOR='\033[0;32m'
 RED_COLOR='\033[0;31m'
 WITHOU_COLOR='\033[0m'
-DELEGATOR_ADDRESS='YOUR_rizon1......ADDRESS'
-VALIDATOR_ADDRESS='YOUR_rizonvaloper1ADDRESS'
+DELEGATOR_ADDRESS='rizon17qltem80lz3gef4a5xgnr9k2kzg8u56axu5q69'
+VALIDATOR_ADDRESS='rizonvaloper17qltem80lz3gef4a5xgnr9k2kzg8u56a4ckpc4'
 PWD='YOUR_PASSWORD'
-DELAY=3200 #in secs - how often restart the script 
+DELAY=36000 #in secs - how often restart the script 
 ACC_NAME='YOU_WALLET_WITHOUT_QUOTES' #example: = ACC_NAME=wallet_qwwq_54
 NODE=http://localhost:26657 #change it only if you use another rpc port of your node
 
 for (( ;; )); do
         echo -e "Get reward from Delegation"
-        echo -e "${PWD}\ny\n" | rizond tx distribution withdraw-all-rewards --from ${ACC_NAME} --fees 20uatolo --chain-id groot-011 --yes
+        echo -e "${PWD}\ny\n" | rizond tx distribution withdraw-all-rewards --from ${ACC_NAME} --fees 20uatolo --chain-id titan-1 --yes
         for (( timer=10; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
@@ -22,7 +22,7 @@ for (( ;; )); do
         # BAL=$(($BAL -100000))
         echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uatolo\n"
         echo -e "Claim rewards\n"
-        echo -e "${PWD}\n${PWD}\n" | rizond tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id groot-011 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 1000uatolo
+        echo -e "${PWD}\n${PWD}\n" | rizond tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id titan-1 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 1000uatolo
         for (( timer=10; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
@@ -32,7 +32,7 @@ for (( ;; )); do
         BAL=$(($BAL -100000))
         echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uatolo\n"
         echo -e "Stake ALL 11111\n"
-        echo -e "${PWD}\n${PWD}\n" | rizond tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}uatolo --chain-id groot-011 --from ${ACC_NAME} --node ${NODE} -y --fees 1000uatolo
+        echo -e "${PWD}\n${PWD}\n" | rizond tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}uatolo --chain-id titan-1 --from ${ACC_NAME} --node ${NODE} -y --fees 1000uatolo
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
