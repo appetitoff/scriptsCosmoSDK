f18db4ad081fc5162f5327e12542ccfc79a9fd86
appetitoff <87865050+appetitoff@users.noreply.github.com>
2021-10-20 15:30:21 +0200

    Update restakingRizon.sh

--- restakingRizon.sh
+++ restakingRizon.sh
@@ -2,16 +2,16 @@
 GREEN_COLOR='\033[0;32m'
 RED_COLOR='\033[0;31m'
 WITHOU_COLOR='\033[0m'
-DELEGATOR_ADDRESS='rizon17qltem80lz3gef4a5xgnr9k2kzg8u56axu5q69'
-VALIDATOR_ADDRESS='rizonvaloper17qltem80lz3gef4a5xgnr9k2kzg8u56a4ckpc4'
-PWD='YOUR_PASSWORD'
-DELAY=36000 #in secs - how often restart the script 
-ACC_NAME='YOU_WALLET_WITHOUT_QUOTES' #example: = ACC_NAME=wallet_qwwq_54
+DELEGATOR_ADDRESS='game...'
+VALIDATOR_ADDRESS='gamevaloper...'
+PWD='pass wallet'
+DELAY=86400 #in secs - how often restart the script 
+ACC_NAME=wallet name 
 NODE=http://localhost:26657 #change it only if you use another rpc port of your node
 
 for (( ;; )); do
         echo -e "Get reward from Delegation"
-        echo -e "${PWD}\ny\n" | rizond tx distribution withdraw-all-rewards --from ${ACC_NAME} --fees 20uatolo --chain-id titan-1 --yes
+        echo -e "${PWD}\ny\n" | rizond tx distribution withdraw-all-rewards --from ${ACC_NAME} --fees 20ugame --chain-id neuron-1 --yes
         for (( timer=10; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
@@ -20,19 +20,19 @@ for (( ;; )); do
 
         BAL=$(rizond query bank balances ${DELEGATOR_ADDRESS} --node ${NODE});
         # BAL=$(($BAL -100000))
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uatolo\n"
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ugame\n"
         echo -e "Claim rewards\n"
-        echo -e "${PWD}\n${PWD}\n" | rizond tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id titan-1 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 1000uatolo
+        echo -e "${PWD}\n${PWD}\n" | nibirud tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id neuron-1 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 5000ugame
         for (( timer=10; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
         done
-        BAL=$(rizond query bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
+        BAL=$(nibirud query bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
         BAL=$(($BAL -100000))
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} uatolo\n"
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ugame\n"
         echo -e "Stake ALL 11111\n"
-        echo -e "${PWD}\n${PWD}\n" | rizond tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}uatolo --chain-id titan-1 --from ${ACC_NAME} --node ${NODE} -y --fees 1000uatolo
+        echo -e "${PWD}\n${PWD}\n" | nibirud tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}ugame --chain-id neuron-1 --from ${ACC_NAME} --node ${NODE} -y --fees 5000ugame
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
